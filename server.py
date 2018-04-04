98474aae24fb17f06ece5110b06074d8d8f53ce7
napier40401627 <notbad1199@gmail.com>
2018-04-03 21:59:42 -0700

    update

--- server.py
+++ server.py
@@ -2,35 +2,57 @@ from flask import Flask, render_template
 import json
 
 w = json.load(open("worldl.json"))
-
+for c in w:
+	c['tld'] = c['tld'][1:]
+page_size = 20
 app = Flask(__name__)
 
 @app.route('/')
 def mainPage():
-    return '<br> '.join([c['name'] for c in w])
+	return render_template('index.html',
+		w = w[0:page_size],
+		page_number = 0,
+		page_size = page_size)
+
+@app.route('/begin/<b>')
+def beginPage(b):
+	bn = int(b)
+	return render_template('index.html',
+		w = w[bn:bn+page_size],
+		page_number = bn,
+		page_size = page_size
+		)
 
 @app.route('/continent/<a>')
 def continentPage(a):
-    #return w[int(i)]['name'] +' : '+ w[int(i)]['continent']
-    cl = [c['name'] for c in w if c['continent']==a]
-    return render_template('continent.html',
-                           loc = len(cl),
-                           cl = cl,
-                           a = a
-                           )
-           
+	cl = [c for c in w if c['continent']==a]
+	return render_template(
+		'continent.html',
+		length_of_cl = len(cl),
+		cl = cl,
+		a = a
+		)
+
 @app.route('/country/<i>')
 def countryPage(i):
-    #return w[int(i)]['name'] +' : '+ w[int(i)]['continent']
-    return render_template('country.html', c = w[int(i)])
+	return render_template(
+		'country.html',
+		c = w[int(i)])
 
 @app.route('/countryByName/<n>')
 def countryByNamePage(n):
-    c = None
-    for x in w:
-        if x['name'] == n:
-            c = x
-    #return w[int(i)]['name'] +' : '+ w[int(i)]['continent']
-    return render_template('country.html', c = c)
+	c = None
+	for x in w:
+		if x['name'] == n:
+			c = x
+	return render_template(
+		'country.html',
+		c = c)
 
-app.run(host='0.0.0.0', port=5627, debug=True)
+app.run(host='0.0.0.0', port=8080, debug=True)
+
+
+
+
+
+
