2f8d7a9a8245fa8a85e6651a0cdc7ed365b1d748
napier40401627 <notbad1199@gmail.com>
2018-04-23 19:46:35 -0700

    alpha

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import json
 w = json.load(open("worldl.json"))
 for c in w:
 	c['tld'] = c['tld'][1:]
+cba = sorted(list(set([c['name'][0] for c in w])))
+print(cba)
 page_size = 20
 app = Flask(__name__)
 
@@ -12,7 +14,8 @@ def mainPage():
 	return render_template('index.html',
 		w = w[0:page_size],
 		page_number = 0,
-		page_size = page_size)
+		page_size = page_size,
+                cba = cba)
 
 @app.route('/begin/<b>')
 def beginPage(b):
@@ -38,6 +41,16 @@ def countryPage(i):
 	return render_template(
 		'country.html',
 		c = w[int(i)])
+
+@app.route('/countrybyalpha/<a>')
+def countrybyalpha(a):
+	cl = [c for c in w if c['name'][0]==a]
+	return render_template(
+		'countrybyalpha.html',
+		length_of_cl = len(cl),
+		cl = cl,
+		a = a
+		)
 
 @app.route('/countryByName/<n>')
 def countryByNamePage(n):
